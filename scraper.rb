3f5fa9f04725ec4b7af1a32f0cb13e4cda1cb602
Owen Stephens <owen@ostephens.com>
2014-10-21 20:30:46 +0100

    More debugging

--- scraper.rb
+++ scraper.rb
@@ -24,11 +24,11 @@ page_add = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 
 page_add.each do |add|
   url = "http://www.bbc.co.uk/radio4/features/book-club/archives/books-" + add.to_s + '/'
-  puts url
   html = ScraperWiki.scrape(url)
-  puts html.to_s
   doc = Nokogiri::HTML(html)
   doc.xpath("//div[@class='box list-promos']/ul/li/a").each do |episodes|
+      puts "got episode"
+      puts episodes.to_s
       episode = Episode.new(episodes.attribute("href"),episodes.xpath("strong").inner_text)
       episode.getAuthor
       ScraperWiki.save(unique_keys=['episodeurl',], data={'episodeurl' => episode.url, 'booktitle' => episode.booktitle, 'author' => episode.bookauthor})
